0dc760bfd87515310a88890b019e64c892562f6f
Anatta336 <sdriver@gmx.co.uk>
2020-03-02 17:48:43 +0000

    sample multiple texels per thread

--- Assets/Scripts/BrightPoint.hlsl
+++ Assets/Scripts/BrightPoint.hlsl
@@ -1,7 +1,8 @@
 struct BrightPoint // 8 * 4 = 32 bytes
 {
-  int2 middle;      // midpoint in texels
-  float magnitude;  // (luminance - luminanceThreshold) / luminanceThreshold
-  float3 colour;    // colour of pixel that spawned it
-  float2 padding;   // pad out to a multiple of float4
+  int2 middle;     // midpoint in texels
+  float magnitude; // (luminance - luminanceThreshold) / luminanceThreshold
+  float3 colour;   // colour of pixel that spawned it
+  float luminance; // 
+  float padding;   // pad out to a multiple of float4
 };
